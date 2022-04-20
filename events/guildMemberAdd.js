efbc0d39c8cc3bfbd06eec586410ffe1fdd86529
NotTCA <realtcatech@gmail.com>
2022-04-20 21:09:23 +0800

    added a cool image into the welcomer

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,26 @@
 const { MessageEmbed } = require("discord.js");
 const client = require("../index");
+const { welcomeImage } = require("ultrax");
 
 client.on("guildMemberAdd", async (member) => {
+  const bg =
+    "https://media.discordapp.net/attachments/965816962062110743/966324282344996924/cool_woodlands_thing_ig_but_with_dark_bg.png";
+  const avatar = member.user.displayAvatarURL({ format: "png" });
+  const options = {
+    attachmentName: `welcome-${member.id}`,
+    text1_fontSize: 80,
+    text2_fontSize: 50,
+    text3_fontSize: 30,
+  };
+  const image = await welcomeImage(
+    bg,
+    avatar,
+    "Welcome",
+    member.user.tag,
+    `You are member #${member.guild.memberCount}`,
+    "#ffffff",
+    options
+  );
   client.channels.fetch(client.config.channels.guestbook).then((channel) => {
     channel.send({
       embeds: [
@@ -10,7 +29,7 @@ client.on("guildMemberAdd", async (member) => {
           .setDescription(
             `Welcome to ${member.guild.name}! Make sure to read the <#${client.config.channels.rules}> and enjoy your stay!`
           )
-          .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+          .setImage(`attachment://${options.attachmentName}.png`)
           .setColor(client.config.color)
           .setFooter({
             text: `Welcomer powered by ${client.user.username}`,
@@ -18,6 +37,7 @@ client.on("guildMemberAdd", async (member) => {
           })
           .setTimestamp(),
       ],
+      files: [image],
     });
   });
 });
